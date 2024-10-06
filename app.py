3c91b71f0466375244818b30b5c0ac2ff7f7b1cc
Daniel Ramolete <ddramolete@student.apc.edu.ph>
2024-10-07 02:57:10 +0800

    Initial AI Sudoku Template

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', puzzle=puzzle)
+
+@app.route('/solve', methods=['POST'])
+def solve():
+    board = request.json['board']
+    if solve_sudoku(board):
+        return jsonify({"solution": board})
+    return jsonify({"solution": None})
+
+def is_valid(board, row, col, num):
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0  # Reset
+                return False
+    return True
+
+if __name__ == '__main__':
+    app.run(debug=True)
